036fe9caf8e55e0371e22ce9f5c7166ef6b88de4
neo-dev <sei133dirsoa@gmail.com>
2026-05-01 12:38:05 -0300

    theme swap to Kanagawa Dragon

--- init.el
+++ init.el
@@ -70,11 +70,10 @@
 (when (fboundp 'emoji-insert)
   (define-key global-map (kbd "C-x 8 e") 'emoji-insert))
 
-;; Catppuccin theme
-(use-package catppuccin-theme
+;; Kanagawa Dragon theme (replaces Catppuccin)
+(use-package kanagawa-themes
   :config
-  (setq catppuccin-flavor 'mocha)
-  (load-theme 'catppuccin :no-confirm))
+  (load-theme 'kanagawa-dragon t))
 
 ;; Doom modeline
 (use-package doom-modeline
@@ -326,6 +325,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(kanagawa-dragon))
+ '(custom-safe-themes
+   '("d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
+     "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077"
+     "c20728f5c0cb50972b50c929b004a7496d3f2e2ded387bf870f89da25793bb44"
+     default))
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
