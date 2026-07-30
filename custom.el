65f43d2502016e01ca5bf23e230cc36fad1fc358
neo-dev <sei133dirsoa@gmail.com>
2026-07-30 18:28:12 -0300

    change default theme to gruvbox light soft

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-material-dark))
+ '(custom-enabled-themes '(gruvbox-light-soft))
  '(custom-safe-themes
    '("fffef514346b2a43900e1c7ea2bc7d84cbdd4aa66c1b51946aade4b8d343b55a"
      "dfb1c8b5bfa040b042b4ef660d0aab48ef2e89ee719a1f24a4629a0c5ed769e8"
